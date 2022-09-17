894a1f79c14a119bc920f713af29ea58d4665fb7
Raif Cinar <raifcinar81@gmail.com>
2022-09-17 22:24:18 +0300

    Added variable usage requirement project.

---
+++ day_1_homeworks/variableUsageRequirement/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // DAY 1 - Variable Usage Requirement
+
+        /* Notes:
+            Java is a case-sensitive programming language.
+            Reusability: Write once, use many times.
+         */
+
+        int numberOfStudents = 22;
+        String text = "Number Of Students : ";
+
+        System.out.println(text + numberOfStudents);
+    }
+}
