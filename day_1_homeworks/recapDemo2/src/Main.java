f5ec7af595a2eab0ba70c75f110a6fe77c9552cd
Raif Cinar <raifcinar81@gmail.com>
2022-09-19 22:46:37 +0300

    added the topic of working with arrays.

--- day_1_homeworks/recapDemo2/src/Main.java
+++ day_1_homeworks/recapDemo2/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         //DAY 1 - Recap Demo 2
-        double[] myList = {1.2, 1.3, 4.3, 5.6};
+        double[] myList = {1.2, 6.3, 4.3, 5.6};
         double total = 0;
         double max = myList[0];
 
