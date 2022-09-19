2baf1ddb471462ecd7be880a5b80d5e2c14375ac
Raif Cinar <raifcinar81@gmail.com>
2022-09-19 22:43:19 +0300

    added the topic of working with arrays.

---
+++ day_1_homeworks/recapDemo2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //DAY 1 - Recap Demo 2
+        double[] myList = {1.2, 1.3, 4.3, 5.6};
+        double total = 0;
+        double max = myList[0];
+
+        for (double number : myList) {
+            total += number;
+
+            if (max < number) max = number;
+        }
+        System.out.println("Total : " + total);
+        System.out.println("Max value : " + max);
+    }
+}
