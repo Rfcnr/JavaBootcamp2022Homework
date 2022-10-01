caf28883454a0b1c8d1e5d10d6353e483013ca84
Raif ÇINAR <raifcinar81@gmail.com>
2022-10-01 20:14:15 +0300

    Delete day_3_homeworks directory

--- day_3_homeworks/day3Homeworks/src/overriding/Main.java
+++
@@ -1,7 +0,0 @@
-package overriding;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("overriding main ...");
-    }
-}
--- day_3_homeworks/day3Homeworks/src/polymorphism/Main.java
+++
@@ -1,7 +0,0 @@
-package polymorphism;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("polymorphizm main ...");
-    }
-}
